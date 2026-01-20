5bc88b282dce802f4b1bb71f915d84ebb4f05625
DrMundo0 <1115351+DrMundo0@users.noreply.github.com>
2026-01-20 15:38:21 +0800

    向量加法运算

--- math/0201.py
+++ math/0201.py
@@ -160,14 +160,15 @@ for i in range(0, len(dino_vectors)):
         print(i, dino_vectors[i], dino_vectors[i + 1])
         objs.append(Segment(dino_vectors[i], dino_vectors[i + 1]))
     elif i == len(dino_vectors) - 1:
-        # 第20个点，从最后
+        # 第20个点，从最后一个向量指向第一个向量
         print(i, dino_vectors[i], dino_vectors[0])
         objs.append(Segment(dino_vectors[i], dino_vectors[0]))
     else:
-        # 第21个点，
+        # 第21个点，从最后一个向量指向前一个向量
         print(i, dino_vectors[i - 1], dino_vectors[i])
         objs.append(Segment(dino_vectors[i - 1], dino_vectors[i]))
 
+# 画恐龙的点和线（笨版本）
 draw(*objs)
 
 # 画恐龙的聪明版本，利用多边形对象
@@ -175,3 +176,16 @@ draw(Polygon(*dino_vectors))
 
 # 画出恐龙的点和线
 draw(Points(*dino_vectors), Polygon(*dino_vectors))
+
+# 定义向量加法运算函数，向量的加法就是组成向量的两个成员分别相加，组成新的向量
+# 加法可以理解为向量的移动，先向 x 轴的左边或者右边移动，再向 y 轴的上边或者下边移动
+def add(v1, v2):
+    return (v1[0] + v2[0], v1[1] + v2[1])
+
+# add((-1.5, -2.5), v) for v in dino_vectors 是一个列表推导式
+# 由两部分构成，先看后面，for v in dino_vectors 循环取出数组 dino_vectors 中的每一个向量
+# 再看前面的部分，add((-1.5, -2.5), v) 是对循环取出的每一个向量要做的操作，这个表达式中的 v 就是后面循环语句中指代每个向量所使用的 v
+# 这两部分加起来之后，将会返回21个都加了 (-1.5, -2.5) 的向量，用中括号括起来，让他们组成一个新的向量数组
+# dino_vectors 中每个向量都加 (-1.5, -2.5) 相当于左移 1.5，再下移 2.5
+dino_vectors2 = [ add((-1.5, -2.5), v) for v in dino_vectors ]
+draw(Points(*dino_vectors2), Polygon(*dino_vectors))
--- math/README.md
+++ math/README.md
@@ -7,3 +7,11 @@
 3. 微积分
 
 4. 信息论与图论
+
+## 程序员数学：用Python学透线性代数和微积分
+
+* [示例代码](https://www.ituring.com.cn/book/2864)
+
+## 统计思维：程序员数学之概率统计（第2版）
+
+* [示例代码](https://www.ituring.com.cn/book/1555)
