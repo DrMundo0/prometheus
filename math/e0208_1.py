c69098e21d8b4226661055e5073fd020385e57fd
1115351+DrMundo0 <1115351+DrMundo0@users.noreply.github.com>
2026-02-12 13:39:43 +0800

    A: Python main 最佳实践

---
+++ math/e0208_1.py
@@ -0,0 +1,26 @@
+from draw2d import draw
+# 恐龙的点坐标在 e0207 中，但是这句 import 会执行其中的 draw 函数
+from e0207 import dino_vectors
+from patch2d import Arrow, Polygon, Points, Segment, add
+
+if __name__ == "__main__":
+    # 绘制队列，用点坐标初始化
+    objs = [ Points(*dino_vectors) ]
+
+    # 将要绘制的线段加入绘制队列中
+    for i in range(0, len(dino_vectors)):
+        if i < len(dino_vectors) - 1:
+            # 从第一个点，到最后一个点，每一个线段都是从自己到下一个点
+            print(i, dino_vectors[i], dino_vectors[i + 1])
+            objs.append(Segment(dino_vectors[i], dino_vectors[i + 1]))
+        elif i == len(dino_vectors) - 1:
+            # 第20个点，从最后一个向量指向第一个向量
+            print(i, dino_vectors[i], dino_vectors[0])
+            objs.append(Segment(dino_vectors[i], dino_vectors[0]))
+        else:
+            # 第21个点，从最后一个向量指向前一个向量
+            print(i, dino_vectors[i - 1], dino_vectors[i])
+            objs.append(Segment(dino_vectors[i - 1], dino_vectors[i]))
+
+    # 画恐龙的点和线（笨版本）
+    draw(*objs)
