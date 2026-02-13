9ccb2c46ba075e4cfe0fe6ecb54fe45c36ec9e68
1115351+DrMundo0 <1115351+DrMundo0@users.noreply.github.com>
2026-02-13 14:29:05 +0800

    A: 图2-22：用-1/2乘以向量的标量乘法

---
+++ math/Fig0222.py
@@ -0,0 +1,18 @@
+import color
+from draw2d import draw
+from Fig0207 import dino_vectors
+from Fig0213 import add
+from Fig0221 import multiply
+from patch2d import Arrow, Polygon, Points, Segment
+
+# 图2-22：用-1/2乘以向量的标量乘法
+if __name__ == "__main__":
+    a = (6, 4)
+    # 如果向量乘以一个负的标量，不仅改变长度，还会改变方向
+    b = multiply(a, -0.5)
+
+    draw(
+        Points(*[ a, b ]),
+        Arrow(a),
+        Arrow(b, color=color.blue)
+    )
