a65d6234107a28e603a572925a2ba71f04d120d1
1115351+DrMundo0 <1115351+DrMundo0@users.noreply.github.com>
2026-09-10 11:06:30 +0800

    feat: Python中的整数和浮点数

---
+++ python/02_number.py
@@ -0,0 +1,44 @@
+# 整数运算
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+
+# 科学运算
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+# 平方
+print(3 ** 2)
+
+# 立方
+print(3 ** 3)
+
+# 六次方
+print(10 ** 6)
+
+# 浮点数运算
+print(0.1 + 0.1)
+print(2 * 0.1)
+
+# 两个数相除，结果总是浮点数
+print(4 / 2)
+
+# 有一个操作数是浮点数，结果就是浮点数
+print(1 + 2.0)
+print(2 ** 3.0)
+print(3.0 ** 2)
+
+# 下划线分割使整数更易读
+universe_age = 14_000_000_000
+# 并不会打印出下划线
+print(universe_age)
+
+# 同时给多个变量赋值
+x, y, z = 0, 0, 0
+
+# 常量表示，Python中没有常量关键字，使用变量名称全大写的约定方式来表示常量
+MAX_CONNECTIONS = 5000
+
+# 打印Python之禅
+import this
